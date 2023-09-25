aef7505012d8bba95ffee566fef5b6fb2f79bd65
sohaibebrahimi <ebrahimi_sa@icloud.com>
2023-09-25 21:52:29 +0300

    tehtava 2 tehty

--- JS kertaus/Tehtava__JS_kertaus__tapahtumankasittely/event-assignments/t2/t2.js
+++ JS kertaus/Tehtava__JS_kertaus__tapahtumankasittely/event-assignments/t2/t2.js
@@ -771,3 +771,86 @@ const restaurants = [
 ];
 
 // your code here
+
+const tableBody = document.querySelector('tbody');
+
+const sortedList = restaurants.sort((a, b) => {
+  return a.name.localeCompare(b.name);
+});
+
+
+sortedList.forEach(item => {
+  const tableRow = document.createElement('tr');
+  const tableCellname = document.createElement('td');
+  const tableCelladdress = document.createElement('td');
+
+  tableCellname.textContent=(item.name);
+  tableCelladdress.textContent=(item.address);
+
+  tableRow.appendChild(tableCellname);
+  tableRow.appendChild(tableCelladdress);
+
+  tableBody.appendChild(tableRow);
+  const closeBtn = document.createElement('button');
+  closeBtn.textContent = 'Close';
+
+  tableCellname.addEventListener("click", (event) => {
+    tableCellname.classList.toggle("highlight");
+
+    const dialog = document.querySelector('dialog');
+
+    if (tableCellname.classList.contains('highlight')) {
+      // Clear the existing content in the dialog
+      dialog.innerHTML = '';
+
+      // Create a table to display restaurant details
+      const table = document.createElement('table');
+      const tableBody = document.createElement('tbody');
+      const tableRow = document.createElement('tr');
+
+      const tableCellname = document.createElement('td');
+      const tableCelladdress = document.createElement('td');
+      const tableCellpostalCode = document.createElement('td');
+      const tableCellcity = document.createElement('td');
+      const tableCellphoneNumber = document.createElement('td');
+      const tableCellcompany = document.createElement('td');
+
+      tableCellname.textContent = item.name;
+      tableCelladdress.textContent = item.address;
+      tableCellpostalCode.textContent = item.postalCode;
+
+      tableCellcity.textContent = item.city;
+      tableCellphoneNumber.textContent = item.phone;
+      tableCellcompany.textContent = item.company;
+
+      tableRow.appendChild(tableCellname);
+      tableRow.appendChild(tableCelladdress);
+      tableRow.appendChild(tableCellpostalCode);
+      tableRow.appendChild(tableCellcity);
+      tableRow.appendChild(tableCellphoneNumber);
+      tableRow.appendChild(tableCellcompany);
+
+      tableBody.appendChild(tableRow);
+      table.appendChild(tableBody);
+
+      dialog.appendChild(table);
+      dialog.appendChild(closeBtn);
+
+      dialog.showModal();
+
+    }
+
+    closeBtn.addEventListener("click", (event) => {
+      tableCellname.classList.remove('highlight')
+      dialog.innerHTML = '';
+      dialog.close();
+    });
+
+  });
+
+
+
+});
+
+
+
